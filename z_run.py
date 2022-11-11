87a83ad9bee13c0cc73af9e04c6922f41d691198
falcon-90125 <sokolov_aa@ngs.ru>
2022-11-11 14:40:53 +0700

    2022-11-11

--- z_run.py
+++ z_run.py
@@ -3,23 +3,20 @@ from price_processing import def_price_df_my, def_price_sale, def_price_my_to_xl
 from pricing_basic import def_pricing_basic
 from pricing_sale import def_pricing_sale
 from to_zakroma import def_to_zakroma
-from config import todays_date, file_directory_resalts, file_name, file_name_sale, file_directory_prices_in_vesta, file_name_prices, file_name_price_LT, file_name_art_dubl, file_directory_to_zakroma, cols_price_df, cols_price_df_sale, cols_in_vesta
-#используем движок xlsxwriter для создания объекта writer. Он и передается функции to_excel().
-#pip install openpyxl xlsxwriter xlrd
-
+from config import todays_date, file_directory_resalts, file_name, file_name_sale, file_directory_prices_in_vesta, file_name_prices, file_name_price_LT, \
+     file_name_art_dubl, cols_price_df, cols_price_df_sale, cols_in_vesta, file_directory_to_zakroma
 
 #Загружаем основной прайс
 price_df = pd.read_excel(file_directory_resalts+file_name, usecols=cols_price_df)
 #Загружаем прайс распродажи
 price_df_sale = pd.read_excel(file_directory_resalts+file_name_sale, usecols=cols_price_df_sale)
 
-# #Обработка прайсов функциями
+#Обработка прайсов функциями
 price_my_to_xlsx = def_price_df_my(price_df) #функция из файла price_processing - преобразование в "мой прайс" и выгрузка его в директорию
 price_public_sale_to_xlsx = def_price_sale(price_df_sale) #функция из файла price_processing - преобразование в "публичный прайс" и выгрузка его в директорию
 
 def_price_my_to_xlsx(price_my_to_xlsx, price_public_sale_to_xlsx, todays_date, file_directory_resalts)
 def_price_public_basic_to_xlsx(price_my_to_xlsx, price_public_sale_to_xlsx, todays_date, file_directory_resalts)
-
 
 #Загружаем файл с ценами из карточки ценообразования
 prices_in_vesta = pd.read_excel(file_directory_prices_in_vesta+file_name_prices, usecols=cols_in_vesta)
